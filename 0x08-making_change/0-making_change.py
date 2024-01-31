9c654cfb0edbfdca3d66cd2cfae611a9045da761
MMahmudd <shametlab@gmail.com>
2024-01-31 13:52:07 +0000

    update tasks

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Interview_Question_on:_fewest_number_of_coins_needed_to
+meet_a_given_amount_total
+"""
+
+
+def makeChange(coins, total):
+    """ fewest_number_of_coins_needed_t_meet_total """
+    if total <= 0:
+        return 0
+    # sort the coins in descending order
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
