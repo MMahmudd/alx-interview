48a760e69542772371dfe35f1e4277f4952907ae
MMahmudd <shametlab@gmail.com>
2024-02-17 12:46:42 -0800

    update tasks

--- 0x08-making_change/0-prime_game.py
+++ 0x08-making_change/0-prime_game.py
@@ -3,6 +3,7 @@
 Interview_Question_on:_fewest_number_of_coins_needed_to
 meet_a_given_amount_total
 """
+
 from __future__ import print_function
 import sys
 
@@ -10,7 +11,7 @@ def makeChange(coins, total):
     """ fewest_number_of_coins_needed_t_meet_total """
     if total <= 0:
         return 0
-    # sort the coins in descending order
+    # sort_the_coins _n_ descending_order
     coins.sort(reverse=True)
     change = 0
     for coin in coins:
@@ -22,3 +23,16 @@ def makeChange(coins, total):
     if total != 0:
         return -1
     return change
+
+def main():
+    coins = [25, 10, 5, 1]
+    total = 63
+
+    result = makeChange(coins, total)
+    if result != -1:
+        print("{} coins needed to make a total of {}".format(result, total))
+    else:
+        print("It's not possible to make the total with the given coins.")
+
+if __name__ == "__main__":
+    main()
