2e959dbbf0c223ddb8361acedf98dae7b79250a6
MMahmudd <shametlab@gmail.com>
2024-02-17 12:43:35 -0800

    update tasks

--- 0x08-making_change/0-prime_game.py
+++ 0x08-making_change/0-prime_game.py
@@ -3,7 +3,8 @@
 Interview_Question_on:_fewest_number_of_coins_needed_to
 meet_a_given_amount_total
 """
-
+from __future__ import print_function
+import sys
 
 def makeChange(coins, total):
     """ fewest_number_of_coins_needed_t_meet_total """
